5b2c7fdd56350024758ce905fb0ea19d912f837e
Aniket Patanwal <aniket.patanwal99@gmail.com>
2024-02-17 21:32:02 +0530

    Sliding window counter

--- rate_limiters.py
+++ rate_limiters.py
@@ -81,3 +81,38 @@ def fixed_window_counter_rate_limit(size=60, threshold=60):
             return func(*args, **kwargs)
         return wrapper
     return decorator
+
+SLIDING_WINDOW_COUNTER = defaultdict(int)
+def rate_limit_exceeded(ip_address, size, threshold):
+    current_ts = time.time()
+    print(f'Current ts: {current_ts}')
+    current_window = current_ts//size
+    previous_window = current_window - 1
+    curr_window_key = f'{ip_address}:{current_window}'
+    prev_window_key = f'{ip_address}:{previous_window}'
+    prev_window_count = SLIDING_WINDOW_COUNTER[prev_window_key]
+    curr_window_count = SLIDING_WINDOW_COUNTER[curr_window_key]
+    print(f'Prev window count: {prev_window_count}\n Curr window count: {curr_window_count}')
+    if curr_window_count >= threshold:
+        return True
+    time_elapsed_in_curr_window = current_ts % size
+    print(f'time elapsed: {time_elapsed_in_curr_window}')
+    percentage_elapsed = time_elapsed_in_curr_window / size
+    print(f'Percentage elapsed: {percentage_elapsed}')
+    current_request_rate = curr_window_count + prev_window_count * (1 - percentage_elapsed)
+    print(f'current request rate: {current_request_rate}')
+    if current_request_rate >= threshold:
+        return True
+    SLIDING_WINDOW_COUNTER[curr_window_key] += 1
+    return False
+    
+def sliding_window_counter_rate_limit(size=60, threshold=5):
+    def decorator(func):
+        def wrapper(*args, **kwargs):
+            ip_address = request.remote_addr
+            if rate_limit_exceeded(ip_address, size, threshold):
+                return 'You have been rate limited!', 429
+            return func(*args, **kwargs)
+        return wrapper
+    return decorator
+            
